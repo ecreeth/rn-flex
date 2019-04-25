3f89f93f11b8631565cbb8334de36a91de2796d9
Luis Miguel Alvarado <luismiguel1730@gmail.com>
2019-04-25 11:20:46 -0400

    add flex property to Flex component

--- index.js
+++ index.js
@@ -6,18 +6,23 @@ const propTypes = {
   direction: PropTypes.oneOf(["row", "row-reverse", "column", "column-reverse"]),
   wrap: PropTypes.oneOf(["nowrap", "wrap", "wrap-reverse"]),
   justify: PropTypes.oneOf(["flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly"]),
-  align: PropTypes.oneOf(["flex-start", "flex-end", "center", "baseline", "stretch"])
+  align: PropTypes.oneOf(["flex-start", "flex-end", "center", "baseline", "stretch"]),
+  flex: PropTypes.number
+};
+
+const defaultProps = {
+  flex: 1
 };
 
 export default function Flex(props) {
-  const { style, direction, wrap, justify, align, children, ...restProps } = props;
+  const { style, direction, wrap, justify, align, children, flex, ...restProps } = props;
 
   const flexStyle = {
     flexDirection: direction,
     flexWrap: wrap,
     justifyContent: justify,
     alignItems: align,
-    flex: 1
+    flex: flex
   };
 
   return (
@@ -28,3 +33,4 @@ export default function Flex(props) {
 }
 
 Flex.propTypes = propTypes;
+Flex.defaultProps = defaultProps;
